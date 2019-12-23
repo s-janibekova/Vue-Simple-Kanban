d47fab0ffbd38ff01f18cb2c69316fab36247296
S_Janybekova <s.t.janibekova@gmail.com>
2019-12-23 19:29:33 +0600

    style updated

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <h3 align="center" class="uk-heading-divider">Kanban Board</h3>
+        <h3 align="center" class="uk-heading-divider heading ">Kanban Board</h3>
+        <h2> SJ</h2>
+        <p> Tutorial from Udemy, however style was updated by me</p>
         <div class="uk-grid-medium uk-child-width-expand@s uk-text-center" uk-grid>
             <!-- <BoardColumn name="Todo" />
             <BoardColumn name="InProgress" />
@@ -24,3 +26,20 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.heading {
+    font-family: 'Inria Serif', serif;
+    background-color: #f6e1e1;
+    border: solid 1px;
+    padding: 2% 0 2% 0;
+    box-shadow: 0 0 10px rgba(0,0,0,0.5);
+}
+
+h2,p {
+     font-family: 'Inria Serif', serif;
+     text-align: center;
+     font-size: 1em;
+
+}
+</style>
